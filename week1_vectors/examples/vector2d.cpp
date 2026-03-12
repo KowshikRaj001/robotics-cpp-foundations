b75d168acadd1a96ceb3dfbc6fbe40066565a664
KowshikRaj A <kaushikprasad2005@gmail.com>
2026-03-12 23:43:32 +0700

    update

--- week1_vectors/examples/vector2d.cpp
+++ week1_vectors/examples/vector2d.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cassert>
+#include <assert.h>
 #include "vector2.h"
 using namespace std;
 
@@ -10,12 +10,13 @@ int main() {
     cin >> a.x >> a.y;  //Get the input for the vector a 
     cin >> b.x >> b.y;  //Get the input for the vector b
 
-    Vector2 c = a.add(a);      //Add the vector a to itself 
-    Vector2 d = b.subtract(a); //Subtract vector from b to a
-    Vector2 e = a.multiply(b); //Multiply the vector a with vector b
+    Vector2 c = a + b; //Add the vector a to b
+    Vector2 d = b - a; //Subtract vector from b to a
+    Vector2 e = a * 2.0; //Multiply the vector a with scalar 2.0
     double f = a.dot(b);       //Calculate the dot product of vector a and vector b 
     double g = a.magnitude();  //Calculate the magnitude of vector a
     Vector2 h = a.normalize(); //Normalize the vector a
+    double dist = a.distance(b); //Calculate the distance between vector a and vector b
 
     std::cout << c.x << " " << c.y << std::endl;
     std::cout << d.x << " " << d.y << std::endl;
@@ -23,8 +24,9 @@ int main() {
     std::cout << f << endl;
     std::cout << g << endl;
     std::cout << h.x << " " << h.y << std::endl;
+    std::cout << dist << std::endl;
 
-    assert(a.magnitude() == 5); // Check if the magnitude of vector v is 5
+    assert(std::abs(a.magnitude() - 0) < 1e-9); // Check if the magnitude of vector v is 0
     
     return 0;
 }
--- week1_vectors/src/vector2.cpp
+++ week1_vectors/src/vector2.cpp
@@ -6,30 +6,34 @@ Vector2::Vector2(double x, double y) {
     this->y = y;
 }
 
-Vector2 Vector2::add(const Vector2& other) {
+Vector2 Vector2::operator+(const Vector2& other) const {
     return Vector2(x + other.x, y + other.y);
 }
 
-Vector2 Vector2::subtract(const Vector2& other) {
+Vector2 Vector2::operator-(const Vector2& other) const {
     return Vector2(x - other.x, y - other.y);
 }
 
-Vector2 Vector2::multiply(const Vector2& other) {
-    return Vector2(x * other.x, y * other.y);
+Vector2 Vector2::operator*(double scalar) const {
+    return Vector2(x * scalar, y * scalar);
 }
 
-double Vector2::dot(const Vector2& other) {
+double Vector2::dot(const Vector2& other) const {
     return (x * other.x + y * other.y);
 }
 
-double Vector2::magnitude() {
+double Vector2::magnitude() const {
     return std::sqrt(x * x + y * y);
 }
 
-Vector2 Vector2::normalize() {
+Vector2 Vector2::normalize() const {
     double mag = magnitude();
     if (mag == 0) {
         return Vector2(0, 0); // Return a zero vector if the magnitude is zero
     }
     return Vector2(x / mag, y / mag);
+}
+
+double Vector2::distance(const Vector2& other) const {
+    return std::sqrt((x-other.x)*(x-other.x) + (y-other.y)*(y-other.y));
 }
